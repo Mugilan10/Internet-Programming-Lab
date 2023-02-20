da27196c560cde19ad7ffb6fdad5700321958a1e
Mugilan <69433139+Mugilan10@users.noreply.github.com>
2023-02-20 16:40:02 +0530

    Ip Assignment 1

---
+++ IpAssignment1.js
@@ -0,0 +1,25 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+document.getElementById("demo").innerHTML = fruits.toString();
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.unshift("Lemon");
+
+const myGirls = ["Cecilie", "Lone"];
+
+const myBoys = ["Emil", "Tobias", "Linus"];
+
+
+
+const myChildren = myGirls.concat(myBoys);
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.sort();
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.sort();
+
+fruits.reverse();
