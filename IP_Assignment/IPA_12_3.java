527e68160a4834db9b90a672db528034ddda5d55
Mugilan <69433139+Mugilan10@users.noreply.github.com>
2023-03-12 23:14:06 +0530

    -

---
+++ IP_Assignment/IPA_12_3.java
@@ -0,0 +1,14 @@
+public class IPA_12_3 {
+  public static void main(String[] args) {
+    Object[] myArray = new Object[5];
+    myArray[0] = "Hello World";
+    myArray[1] = 123;
+    myArray[2] = 3.14; 
+    myArray[3] = true;myArray[4] = 'c';
+    System.out.println(myArray[0]); 
+    System.out.println(myArray[1]); 
+    System.out.println(myArray[2]); 
+    System.out.println(myArray[3]); 
+    System.out.println(myArray[4]); 
+  }
+}
